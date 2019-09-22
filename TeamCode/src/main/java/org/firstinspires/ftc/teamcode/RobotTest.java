d04dd89e68524d97258ce94cd0271c7f378f6ec0
Sreehari <sreeharisreedev1@gmail.com>
2019-09-22 16:11:02 -0700

    added movement, strafing, and rotation 🥰

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Barebones", group="Linear Opmode")
+public class RobotTest extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+    private double leftFrontPower;
+    private double leftBackPower;
+    private double rightFrontPower;
+    private double rightBackPower;
+    double drive;
+    double strafe;
+    double rotate;
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()) {
+            drive = -gamepad1.left_stick_y; //forward and backward
+            strafe = gamepad1.right_stick_x; // side to side and diagonal
+            rotate = gamepad1.left_stick_x; // rotate in place
+            /* * * * * * * * * * * *
+             * Left stick:
+             * - up and down moves forwards and backwards
+             * - left and right rotates the robot in place
+             * Right stick:
+             * - strafing side to side
+             * apparently you can face forward while moving in a circle
+             * by strafing and rotating at the same time xDDDDDDD
+             */
+            leftFrontPower = drive + strafe + rotate;
+            leftBackPower = drive - strafe + rotate;
+            rightFrontPower = drive - strafe - rotate;
+            rightBackPower = drive + strafe - rotate;
+
+            leftFront.setPower(leftFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightFront.setPower(rightFrontPower);
+            rightBack.setPower(rightBackPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left front (%.2f), left back (%.2f), right front (%.2f), right back (%.2f)", leftFrontPower, leftBackPower, rightFrontPower, rightBackPower);
+            telemetry.update();
+        }
+    }
+}
