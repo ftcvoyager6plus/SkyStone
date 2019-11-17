9214c5d6a5e62ea8bf35795afa4b288ddfa4d6d6
Sreehari S <sreeharisreedev1@gmail.com>
2019-11-16 16:11:04 -0800

    this version should work...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -9,10 +9,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class AutonomousMode extends LinearOpMode {
     VoyagerBot robot = new VoyagerBot();
     private ElapsedTime runtime = new ElapsedTime();
-    static final double COUNTS_PER_MOTOR_REV = 134.4;
+    static final double COUNTS_PER_MOTOR_REV = 537.6;
     static final double DRIVE_GEAR_REDUCTION = 19.2;
     static final double WHEEL_RADIUS_INCHES = 10 / 2.54;
-    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (2 * WHEEL_RADIUS_INCHES * Math.PI);
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_RADIUS_INCHES * Math.PI);
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
